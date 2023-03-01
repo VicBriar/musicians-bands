ae6c87548d46df1ced872dea1f7e9fa6d4d4d978
Victoria McCrady <spartanginger@gmail.com>
2023-03-01 13:58:39 -0800

    extra credit tests compltete & passing

--- Band.js
+++ Band.js
@@ -3,7 +3,11 @@ const {Sequelize, sequelize, DataTypes} = require('./db');
 // TODO - define the Band model
 let Band = sequelize.define("Band",{
     name: DataTypes.TEXT,
-    genre: DataTypes.TEXT
+    genre: DataTypes.TEXT,
+    showCount: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0
+    }
 })
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -15,6 +15,7 @@ describe('Band and Musician Models', () => {
         const testBand = await Band.create({name: "Dodie", genre: "indie"})
         expect(testBand.name).toBe('Dodie');
         expect(testBand.genre).toBe('indie');
+        expect(testBand.showCount).toBe(0)
     });
 
     test('can create a Musician', async () => {
@@ -23,4 +24,48 @@ describe('Band and Musician Models', () => {
         expect(testMusician.name).toBe("Dorothy Miranda Clark");
         expect(testMusician.instrument).toBe("Voice");
     });
+
+    test('can update Band', async () => {
+        const testBand1 = await Band.create({name: "lankinPirk", genre: "roock", showCount: 100})
+        testBand1.set({
+            name: "Linkin Park",
+            genre: "rock",
+            })
+            testBand1.showCount++
+
+        await testBand1.save();    
+        expect(testBand1.name).toBe("Linkin Park");
+        expect(testBand1.genre).toBe("rock");
+        expect(testBand1.showCount).toBe(101);
+    })
+    test('can update Musician', async () => {
+        const testMusician1 = await Musician.create({name: "Rab Bourdon", instrument: "Drumies"})
+        testMusician1.set({
+            name: "Rob Bourdon",
+            instrument: "Drums",
+        });
+        await testMusician1.save();
+        expect(testMusician1.name).toBe("Rob Bourdon");
+        expect(testMusician1.instrument).toBe("Drums");
+    });
+    test('can Destroy Band', async () => {
+        const testBand2 = await Band.create({name: "lankinPirk", genre: "roock", showCount: 100})
+        const id = testBand2.id;
+        await testBand2.destroy();
+        const band2 = await Band.findByPk(id);
+
+        expect(band2).toBe(null);
+
+    })
+    test('can destory Musician',async () => {
+        const testMusician2 = await Musician.create({name: "Rab Bourdon", instrument: "Drumies"})
+        const id = testMusician2.id;
+        await testMusician2.destroy()
+        const musician2 = await Musician.findByPk(id)
+
+        expect(musician2).toBe(null);
+
+    })
+
+        
 })
